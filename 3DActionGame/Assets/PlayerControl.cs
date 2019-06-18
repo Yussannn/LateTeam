59999ffc3793dad9585976fab2198d51a1cc9746
yussan0701 <m.yuuki071@gmail.com>
2019-06-18 16:28:59 +0900

    初回

---
+++ 3DActionGame/Assets/PlayerControl.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private CharacterController p_contorol;
+    private Animator animCon;
+    public float speed = 5.0f;
+    public float rotationSpeed = 1200.0f;
+
+    private const float gravityPower = 9.8f;
+    public Rigidbody rb;
+
+    public void Hit()
+    {
+    }
+
+    void Start()
+    {
+        p_contorol = GetComponent<CharacterController>();
+        animCon = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if(Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)
+        {
+            animCon.SetBool("Walking", false);
+        }
+
+        else
+        {
+            var cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+            Vector3 direction = cameraForward * Input.GetAxis("Vertical") + Camera.main.transform.right * Input.GetAxis("Horizontal");
+            animCon.SetBool("Walking", true);
+
+            ChangeDirection(direction);
+            Move(direction);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(0, 5, 0);
+        }
+
+        //アクション処理を追加していく
+        // 例 : animCon.SetBool("Action", Input.GetKey("x") || Input.GetButtonDown("Action"));
+
+    }
+
+    void ChangeDirection(Vector3 dir)
+    {
+        Quaternion q = Quaternion.LookRotation(dir);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, rotationSpeed * Time.deltaTime);
+    }
+
+    void Move(Vector3 dis)
+    {
+        rb.AddForce(dis * Time.deltaTime * speed);
+    }
+}
---
+++ 3DActionGame/Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 3f;
+    public float thrust = 200;
+    private Animator anim;
+    Vector3 p_Pos;
+    bool ground;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        p_Pos = transform.position;
+    }
+
+    void Update()
+    {
+        if (ground)
+        {
+            float x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+            float z = Input.GetAxisRaw("Vertical") * Time.deltaTime * speed;
+
+            rb.MovePosition(transform.position + new Vector3(x, 0, z));
+            Vector3 dir = transform.position - p_Pos;
+            if (dir.magnitude > 0.01f)
+            {
+                transform.rotation = Quaternion.LookRotation(new Vector3(dir.x, 0, dir.z));
+                anim.SetBool("Running", true);
+            }
+            else
+            {
+                anim.SetBool("Running", false);
+            }
+
+            p_Pos = transform.position;
+            if (Input.GetButton("Jump"))
+            {
+                rb.AddForce(transform.up * thrust);
+                if(rb.velocity.magnitude > 0)
+                {
+                    rb.AddForce(transform.forward * thrust + transform.up * thrust / 10);
+                }
+            }
+        }
+
+    }
+    void OnCollisionStay(Collision col)
+    {
+        ground = true;
+        anim.SetBool("Jumping", false);
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        ground = false;
+        anim.SetBool("Jumping", true);
+    }
+}
