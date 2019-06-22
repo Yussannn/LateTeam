b53435c1fef36b30edefaef94f38c3a64c924c13
syabc <shunsuke.lego.1996@docomo.ne.jp>
2019-06-22 13:55:27 +0900

    シーンの作成

---
+++ 3DActionGame/Assets/Scripts/GameSystem2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSystem2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void GameEnd()
+    {
+        SceneManager.LoadScene("SampleScene._title");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 3DActionGame/Assets/Scripts/TimerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    public int TimerCount;
+
+    public Text TimerText;
+
+    public float totalTime;
+
+    int seconds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("GoTitle", TimerCount); 
+    }
+
+    void GoTitle()
+    {
+        SceneManager.LoadScene("SampleScene_win");
+    }     
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalTime -= Time.deltaTime;
+        seconds = (int)totalTime;
+        TimerText.text = seconds.ToString();
+    }
+}
