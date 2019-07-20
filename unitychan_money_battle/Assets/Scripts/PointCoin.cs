2ab8dfe39cfe3d498324ef9e14293695b4c13660
yussan0701 <m.yuuki071@gmail.com>
2019-07-20 19:17:38 +0900

    コイン取るとコインが消えるように、ポイントスクリプトを追加。 [メモ] やること 2Pを追加、タイトル→ゲーム説明→プレイ→リザルト→タイトル の流れを作ること

---
+++ unitychan_money_battle/Assets/Scripts/PointCoin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointCoin : MonoBehaviour
+{
+    int playerPoint = 0;
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "goldcoin")
+        {
+            Destroy(col.gameObject);
+            playerPoint += 2;
+        }
+        else if(col.gameObject.name == "silvercoin")
+        {
+            Destroy(col.gameObject);
+            playerPoint += 1;
+        }
+    }
+}
