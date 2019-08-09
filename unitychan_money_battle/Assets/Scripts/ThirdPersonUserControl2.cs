a720ba97b1ea1bfb09fd737905749b0c9f4f4ff5
yussan0701 <m.yuuki071@gmail.com>
2019-08-09 16:31:28 +0900

    原因が分かった。とりあえず、ちょっと待って

--- unitychan_money_battle/Assets/Scripts/ThirdPersonUserControl2.cs
+++ unitychan_money_battle/Assets/Scripts/ThirdPersonUserControl2.cs
@@ -46,8 +46,8 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         private void FixedUpdate()
         {
             // read inputs
-            float h = CrossPlatformInputManager.GetAxis("Player2_Horizontal");
-            float v = CrossPlatformInputManager.GetAxis("Player2_Vertical");
+            float h = CrossPlatformInputManager.GetAxis("L_XAxis_2");
+            float v = CrossPlatformInputManager.GetAxis("L_YAxis_2");
             bool crouch = Input.GetKey(KeyCode.C);
 
             // calculate move direction to pass to character
@@ -55,12 +55,12 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             {
                 // calculate camera relative direction to move:
                 m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-                m_Move = v * m_CamForward + h * m_Cam.right;
+                m_Move = -v * m_CamForward + h * m_Cam.right;
             }
             else
             {
                 // we use world-relative directions in the case of no main camera
-                m_Move = v * Vector3.forward + h * Vector3.right;
+                m_Move = -v * Vector3.forward + h * Vector3.right;
             }
 #if !MOBILE_INPUT
             // walk speed multiplier
--- unitychan_money_battle/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ unitychan_money_battle/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -1,7 +1,7 @@
-using System;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 using GamepadInput;
+namespace GamepadInput{ }
 
 namespace UnityStandardAssets.Characters.ThirdPerson
 {
@@ -47,10 +47,8 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         private void FixedUpdate()
         {
             // read inputs
-            float h = CrossPlatformInputManager.GetAxis("Player1_Horizontal");
-            h = Input.GetAxis("Player1_Horizontal");
-            float v = CrossPlatformInputManager.GetAxis("Player1_Vertical");
-            v = Input.GetAxis("Player1_Vertical");
+            float h = CrossPlatformInputManager.GetAxis("L_XAxis_1");
+            float v = CrossPlatformInputManager.GetAxis("L_YAxis_1");
             bool crouch = Input.GetKey(KeyCode.C);
 
             // calculate move direction to pass to character
@@ -58,12 +56,12 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             {
                 // calculate camera relative direction to move:
                 m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-                m_Move = v*m_CamForward + h*m_Cam.right;
+                m_Move = -v*m_CamForward + h*m_Cam.right;
             }
             else
             {
                 // we use world-relative directions in the case of no main camera
-                m_Move = v*Vector3.forward + h*Vector3.right;
+                m_Move = -v*Vector3.forward + h*Vector3.right;
             }
 #if !MOBILE_INPUT
 			// walk speed multiplier
