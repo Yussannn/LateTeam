2acef247670af66467f38cad50acfe19bf1d8d29
yussan0701 <m.yuuki071@gmail.com>
2019-10-16 06:03:41 +0900

    InputDevice一個多かったらから消した

---
+++ unitychan_money_battle/Assets/VariableDifinition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableDifinition : MonoBehaviour
+{
+    public static GameObject player1, player2;
+    public static GameObject sub_camera1, sub_camera2;
+
+    void Start()
+    {
+        player1 = GameObject.Find("UnityChanPlayer0");
+        player2 = GameObject.Find("UnityChanPlayer1");
+        sub_camera1 = GameObject.Find("1P_SubCamera");
+        sub_camera2 = GameObject.Find("2P_SubCamera");
+    }
+}
